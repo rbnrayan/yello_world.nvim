20c2bb6ed69f19eb8c794cecf56d25074ae6ab30
rbnrayan <rayan.robion7@gmail.com>
2022-08-22 11:23:59 +0200

    add fg_alt

--- lua/yello_world/palette.lua
+++ lua/yello_world/palette.lua
@@ -5,12 +5,13 @@ local colors = {
   dark_alt = "#232323",
   gray = "#787878",
   white = "#fefefe",
+  black = "#080808",
   yellow = "#e7e055",
   green = "#93e06c",
   error = "#e05151",
   warning = "#e0a451",
   blue = "#2758cc",
-  darker_green = "#2fbf28",
+  darker_green = "#25b01e",
 }
 
 return colors
